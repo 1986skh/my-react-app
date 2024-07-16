2567f043104f691f7d9ee33b2e92da861bd06c7c
1986skh <khelifa.smail08@gmail.com>
2024-07-16 20:58:10 +0200

    first commit

--- src/compenents/PokemonCard.jsx
+++ src/compenents/PokemonCard.jsx
@@ -1,25 +1,14 @@
 
-
-
-function App() {
-  
-
-  return (
-    <>
-    
-      <div>
-
-      <h1> hello react</h1>
-      <figure>
+function PokemonCard() {
+    return (
+        <figure >
             <img 
                 src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
                 alt="Pokemon Image"
             />
             <figcaption>Numéro 1 - Bulbasaur</figcaption>
         </figure>
-      </div>
-    </>
-  );
+    );
 }
 
-export default App;
+export default PokemonCard;
